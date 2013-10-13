70792f645865d32e637960647ebb2df1ffaf3782
willpdp <palmer.w+github@gmail.com>
2013-10-13 21:16:41 +0100

    Fix slight difference

--- codemp/rd-vanilla/G2_API.cpp
+++ codemp/rd-vanilla/G2_API.cpp
@@ -2617,7 +2617,7 @@ qboolean G2_TestModelPointers(CGhoul2Info *ghlInfo) // returns true if the model
 						{
 							if (ghlInfo->currentAnimModelSize!=ghlInfo->aHeader->ofsEnd)
 							{
-						  Com_Error(ERR_DROP, "Ghoul2 model was reloaded and has changed, map must be restarted.\n");
+								Com_Error(ERR_DROP, "Ghoul2 model was reloaded and has changed, map must be restarted.\n");
 							}
 						}
 						ghlInfo->currentAnimModelSize=ghlInfo->aHeader->ofsEnd;
@@ -2716,7 +2716,7 @@ qboolean G2_SetupModelPointers(CGhoul2Info *ghlInfo) // returns true if the mode
 						{
 							if (ghlInfo->currentAnimModelSize!=ghlInfo->aHeader->ofsEnd)
 							{
-						  Com_Error(ERR_DROP, "Ghoul2 model was reloaded and has changed, map must be restarted.\n");
+								Com_Error(ERR_DROP, "Ghoul2 model was reloaded and has changed, map must be restarted.\n");
 							}
 						}
 						ghlInfo->currentAnimModelSize=ghlInfo->aHeader->ofsEnd;
